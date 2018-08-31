b66e02909be3e0dd03e88621b3ea4fb189906124
Alex <sokoliuk.av@gmail.com>
2018-08-31 18:29:46 +0300

    work gallery

--- camagru.sql
+++ camagru.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Авг 12 2018 г., 04:06
--- Версия сервера: 5.7.22
--- Версия PHP: 7.1.19
+-- Время создания: Авг 31 2018 г., 08:28
+-- Версия сервера: 5.7.23
+-- Версия PHP: 7.1.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,25 +25,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `luser_like`
+-- Структура таблицы `comments`
 --
 
-CREATE TABLE `luser_like` (
-  `id_image` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `comment` varchar(100) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `id_img` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `comments`
+--
+
+INSERT INTO `comments` (`id`, `comment`, `user_name`, `id_img`) VALUES
+(1, 'fsdfds', '148', 12),
+(2, 'das', '148', 12),
+(3, 'fsd', '148', 16),
+(8, '4rfwfewfew', 'alex', 12),
+(9, 'csds', 'alex', 12),
+(10, 'vxcvcxvcx', 'alex', 17);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `photo`
+-- Структура таблицы `img`
 --
 
-CREATE TABLE `photo` (
-  `id_photo` int(11) NOT NULL,
-  `photo_path` varchar(100) NOT NULL,
-  `id_user` int(11) NOT NULL
+CREATE TABLE `img` (
+  `id_img` int(11) NOT NULL,
+  `src_img` varchar(100) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `img`
+--
+
+INSERT INTO `img` (`id_img`, `src_img`, `id_user`, `date`) VALUES
+(12, 'template/image/1534863372.png', 148, '2018-08-21 07:56:12'),
+(16, 'template/image/1535709504.png', 148, '2018-08-31 02:58:24'),
+(17, 'template/image/1535723579.png', 148, '2018-08-31 06:52:59');
 
 -- --------------------------------------------------------
 
@@ -65,20 +89,34 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `token`, `status`) VALUES
-(122, 'fds', 'alex1@gmail.com', '123456', 'IOPUETR', 0),
-(132, 'alex', 'alex1@gmail.comq', '123456', 'PWOEIRT', 0),
-(139, 'alex', 'sokoliuk.av@gmail.com', '123456', 'EURYIPW', 1),
-(140, 'qqq', '123sdfdf@erere.er', 'qwewewr', 'PREUOYW', 0);
+(148, 'alex', 'sokoliuk.av@gmail.com', '123456', 'IEUQOWR', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_like`
+--
+
+CREATE TABLE `user_like` (
+  `id_img` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `photo`
+-- Индексы таблицы `comments`
 --
-ALTER TABLE `photo`
-  ADD PRIMARY KEY (`id_photo`);
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `img`
+--
+ALTER TABLE `img`
+  ADD PRIMARY KEY (`id_img`);
 
 --
 -- Индексы таблицы `users`
@@ -92,16 +130,22 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT для таблицы `photo`
+-- AUTO_INCREMENT для таблицы `comments`
 --
-ALTER TABLE `photo`
-  MODIFY `id_photo` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT для таблицы `img`
+--
+ALTER TABLE `img`
+  MODIFY `id_img` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
